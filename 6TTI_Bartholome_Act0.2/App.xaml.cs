64dbb1c8e7068751c27dfaa0f769d4037655d661
noe-root <250055@site.asty-moulin.be>
2026-09-16 11:48:03 +0200

    Ajout des projets WPF Act0.1 et Act0.2

---
+++ 6TTI_Bartholome_Act0.2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _6TTI_Bartholome_Act0._2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
